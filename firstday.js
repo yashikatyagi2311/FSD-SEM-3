421cb071a6fcde444961ab438e7f17adef4a5447
yashika tyagi <yashikatyagi2311@gmail.com>
2026-07-24 09:04:50 +0530

    basicjava.js added

---
+++ firstday.js
@@ -0,0 +1,21 @@
+const c="hello";
+console.log(c);
+//datatype
+let a= 23;
+let b= "yashika";
+let d= true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+let e= null;
+console.log(typeof e)
+//operators
+let f=3;
+let g=4;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a%b);
+console.log(a/b);
+
+
